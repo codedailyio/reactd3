d3d654505bb0f8955ee2f52c0512f6695367457e
Jason Brown <browniefed@gmail.com>
2018-02-27 23:08:15 -0800

    Add in tags

--- articles/vx_linepath.md
+++ articles/vx_linepath.md
@@ -3,4 +3,6 @@ path: "/vx/linepath"
 date: "2018-02-27"
 library: "vx"
 title: "Use VX to Render a Line Graph in React"
+tags: ["react", "vx"]
+published: true
 ---
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,20 @@
 const path = require("path");
+const _ = require("lodash");
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
   const blogPostTemplate = path.resolve(`./src/templates/blogTemplate.js`);
+  const tagTemplate = path.resolve("src/templates/tagTemplate.js");
 
   return graphql(`
     {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 2000) {
         edges {
           node {
             frontmatter {
               path
+              tags
             }
           }
         }
@@ -22,11 +25,35 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       return Promise.reject(result.errors);
     }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges;
+
+    // Create post detail pages
+    posts.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
         component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
+      });
+    });
+
+    // Tag pages:
+    let tags = [];
+    // Iterate through each post, putting all found tags into `tags`
+    _.each(posts, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags);
+      }
+    });
+    // Eliminate duplicate tags
+    tags = _.uniq(tags);
+
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
       });
     });
   });
